a65484d87d7699956297af346743775a0a65e0d8
wawawa0719 <tabletennistokei@gmail.com>
2024-11-20 12:43:44 +0900

    first

---
+++ Myapp/gemini.py
@@ -0,0 +1,18 @@
+﻿from discord import welcome_screen
+import google.generativeai as genai
+import os
+# API-KEYの設定
+
+menSetText = "男前な男性"
+womenSetText = "小学生"
+
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+gemini_pro = genai.GenerativeModel("gemini-1.5-flash")
+
+
+def geminiCreateText(inputText):
+  prompt = "以下に示す文章に、ルルーシュ・ランペルージのような口調で返信してください。「" + inputText + "」"
+  response = gemini_pro.generate_content(prompt)
+  print(response.text)
+  return (response.text)
---
+++ Myapp/main.py
@@ -0,0 +1,37 @@
+﻿import discord
+import os
+from keep import keep_alive
+from gemini import geminiCreateText
+
+
+class MyClient(discord.Client):
+    global isStartingUp 
+    isStartingUp = False
+    async def on_ready(self):
+        print(f'ログインしました: {self.user}')
+    async def on_message(self, message):
+        print(f'送信: {message.author}: {message.content}')
+        global isStartingUp
+        if message.author == self.user:
+            return
+        if str(self.user.id) in message.content:
+            isStartingUp = not isStartingUp
+            if isStartingUp:
+                await message.channel.send("はーい")
+            else:
+                await message.channel.send("ばいばい")
+        elif isStartingUp:
+            await message.channel.send(geminiCreateText(message.content))
+
+            
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+keep_alive()
+try:
+    client.run(os.environ['TOKEN'])
+except:
+    os.system("kill")
